d22421740e378036c80a20d7e276f2ff5f331094
Martin Ueding <dev@martin-ueding.de>
2015-01-26 15:55:49 +0100

    Write more stuff

--- young.php
+++ young.php
@@ -3,6 +3,7 @@
 
 $a_text = $_POST['A'];
 $b_text = $_POST['B'];
+$n = (int) $_POST['N'];
 
 if (count($a_text) > 20 || count($a_text) > 20) {
     die('Input too much. I do not want to consume all the resources of my server. :-)');
@@ -77,10 +78,49 @@ function add_block($a2, $b_row_id, $b_col_id) {
 }
 
 function is_legal(&$a) {
+    # If a colum has more than N entries, it is not legal.
+    if (count($a) > $n) {
+        return false;
+    }
+
     # If a row of A is as long as the previous row, this is not legal.
     for ($a_row_id = 0; $a_row_id < count($a); $a_row_id++) {
         if ($a_row_id > 0 && count($a[$a_row_id-1]) <= count($a[$a_row_id])) {
             return false;
         }
     }
+
+    # If letters appear twice in the same column, it is not legal.
+    for ($a_col_id = 0; $a_col_id < count($a[0]); $a_col_id++) {
+        $letters = array();
+        for ($a_row_id = 0; $a_row_id < count($a) && count($a[$a_row_id]) > $a_col_id; $a_row_id++) {
+            $letter = $a[$a_row_id][$a_col_id];
+            if ($letter > $letters) { # TODO
+                return false;
+            }
+            else {
+                $letters[] = $letters;
+            }
+        }
+    }
+
+    # Check ← ↓ traversal.
+    $counts = array();
+    for ($a_row_id = 0; $a_row_id < count($a); $a_row_id++) {
+        for ($a_col_id = count($a[$a_row_id]) - 1; $a_col_id >= 0; $a_col_id--) {
+            $letter = $a[$a_row_id][$a_col_id];
+            $ord = ord($letter) - ord('a');
+            $counts[$ord]++;
+
+            if (count($counts) > 1) {
+                for ($count_id = 1; $count_id < count($counts); $count_id++) {
+                    if ($counts[$count_id - 1] < $counts[$count_id]) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
 }
